e8d0444226227261426cc2fee0c3ecc823aa2e1d
ERIC KELLY <eakelly220@gmail.com>
2018-03-26 04:58:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested div div div .target')
+}
+
+function increaseRankBy(n){
+const lis = document.querySelectorAll('ul.ranked-list li')
+var ammount =lis.length 
+for (let i = 0; i < ammount; i++) {
+  lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n)
+}
+}
+
+function deepestChild(){
+  return document.querySelector("#grand-node div div div div")
+}
